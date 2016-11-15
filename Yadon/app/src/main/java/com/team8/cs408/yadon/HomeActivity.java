da3cba3bdf60b77dd8b62f911d6d50a4ef34ab28
bcue250 <umsukgod@hanmail.net>
2016-11-16 00:30:44 +0900

    Simple low quality log-in screen Kakao link api test

--- Yadon/app/src/main/java/com/team8/cs408/yadon/HomeActivity.java
+++ Yadon/app/src/main/java/com/team8/cs408/yadon/HomeActivity.java
@@ -3,10 +3,37 @@ package com.team8.cs408.yadon;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.kakao.kakaolink.KakaoLink;
+import com.kakao.kakaolink.KakaoTalkLinkMessageBuilder;
+import com.kakao.util.KakaoParameterException;
+
 public class HomeActivity extends AppCompatActivity {
+    private KakaoLink mKakaoLink;
+    private KakaoTalkLinkMessageBuilder mKakaoTalkLinkMessageBuilder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        InitKakaoLink();
+        SendKakaoMessage();
+    }
+
+    private void InitKakaoLink() {
+        try {
+            mKakaoLink = KakaoLink.getKakaoLink(this);
+            mKakaoTalkLinkMessageBuilder = mKakaoLink.createKakaoTalkLinkMessageBuilder();
+        } catch (KakaoParameterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void SendKakaoMessage() {
+        try {
+            mKakaoTalkLinkMessageBuilder.addText("TEST 메시지");
+            mKakaoLink.sendMessage(mKakaoTalkLinkMessageBuilder, this);
+        } catch (KakaoParameterException e) {
+            e.printStackTrace();
+        }
     }
 }
