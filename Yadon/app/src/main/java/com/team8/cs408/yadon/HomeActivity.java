1c9b20e2159f45602f373e0a5d17adfe11ad10de
bcue250 <umsukgod@hanmail.net>
2016-11-27 19:57:49 +0900

    implemented getting the contacts' list with a button Show the contact that is selected

--- Yadon/app/src/main/java/com/team8/cs408/yadon/HomeActivity.java
+++ Yadon/app/src/main/java/com/team8/cs408/yadon/HomeActivity.java
@@ -1,39 +1,53 @@
 package com.team8.cs408.yadon;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.media.VolumeProviderCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import com.kakao.kakaolink.KakaoLink;
-import com.kakao.kakaolink.KakaoTalkLinkMessageBuilder;
-import com.kakao.util.KakaoParameterException;
+import static android.R.attr.name;
+
 
 public class HomeActivity extends AppCompatActivity {
-    private KakaoLink mKakaoLink;
-    private KakaoTalkLinkMessageBuilder mKakaoTalkLinkMessageBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        InitKakaoLink();
-        SendKakaoMessage();
+        ImageButton addGroupButton = (ImageButton) findViewById(R.id.addGroup);
+        addGroupButton.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 
-    private void InitKakaoLink() {
-        try {
-            mKakaoLink = KakaoLink.getKakaoLink(this);
-            mKakaoTalkLinkMessageBuilder = mKakaoLink.createKakaoTalkLinkMessageBuilder();
-        } catch (KakaoParameterException e) {
-            e.printStackTrace();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String name;
+        String number;
+        if(resultCode == RESULT_OK) {
+            Cursor cursor = getContentResolver().query(data.getData(),
+                    new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                            ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null);
+            cursor.moveToFirst();
+            name = cursor.getString(0);
+            number = cursor.getString(1);
+            cursor.close();
+            TextView name_view = (TextView) findViewById(R.id.HometextView1);
+            TextView phonenum_view = (TextView) findViewById(R.id.HometextView2);
+            name_view.setText(name);
+            phonenum_view.setText(number);
         }
-    }
-
-    private void SendKakaoMessage() {
-        try {
-            mKakaoTalkLinkMessageBuilder.addText("TEST 메시지");
-            mKakaoLink.sendMessage(mKakaoTalkLinkMessageBuilder, this);
-        } catch (KakaoParameterException e) {
-            e.printStackTrace();
-        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
